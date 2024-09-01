195ead40e8be6907fb95a6d0ec41c72f277ea54e
이상희 <leesanghee0317@gmail.com>
2024-09-01 20:21:30 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.50 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer=0;
+       
+        Arrays.sort(numbers);
+
+       if(numbers[0]*numbers[1]>
+          numbers[numbers.length-1]*numbers[numbers.length-2])
+           answer=numbers[0]*numbers[1];
+        else answer=numbers[numbers.length-1]*numbers[numbers.length-2];
+        
+        return answer;
+    }
+}
