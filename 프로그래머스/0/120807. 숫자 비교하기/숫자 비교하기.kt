8bcf00c0d91d2e050ebc818667a070f5220c1583
이상희 <leesanghee0317@gmail.com>
2024-09-10 13:53:17 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 59.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(num1: Int, num2: Int): Int {
+        var answer: Int =-1
+        if(num1==num2){
+            answer=1
+        }
+        return answer
+    }
+}
