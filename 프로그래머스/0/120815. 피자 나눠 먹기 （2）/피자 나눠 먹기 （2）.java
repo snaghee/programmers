81124e6a315ab3b4f0f3e8d9cd4a9de341697898
이상희 <leesanghee0317@gmail.com>
2024-10-20 21:50:14 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.04 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+        while (true) {
+            if ((6 * answer) % n == 0) {
+                return answer;
+            }
+            answer++;
+        }
+    }
+}
